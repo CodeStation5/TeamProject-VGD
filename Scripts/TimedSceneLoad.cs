c45a55d8c51bb59ac752b36fbe5f49dc5ba0b120
Jerry Sandhu <35089853+CodeStation5@users.noreply.github.com>
2025-01-29 01:40:04 -0800

    Create TimedSceneLoad.cs

---
+++ Scripts/TimedSceneLoad.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimedSceneLoad : MonoBehaviour
+{
+    public CanvasGroup canvasgroup;
+    [SerializeField]
+    public float timerDelay = 44f;
+    [SerializeField]
+    public string sceneLoad;
+    private float timeElapsed;
+    public bool fadeInBlack;
+    public bool fadeOutBlack;
+    public float fadeTime;
+    public bool isTimed;
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed >= (timerDelay - fadeTime) && isTimed == true)
+        {
+            if (fadeInBlack == true)
+            {
+                canvasgroup.alpha = 1;
+            }
+            if (fadeOutBlack == true)
+            {
+                canvasgroup.alpha = 0;
+            }
+            if (timeElapsed > timerDelay && isTimed == true)
+            {
+                SceneManager.LoadScene(sceneLoad);
+            }
+        }
+    }
+
+    public void FadeIn()
+    {
+        fadeInBlack = true;
+    }
+    public void FadeOut()
+    {
+        fadeOutBlack = true;
+    }
+}
